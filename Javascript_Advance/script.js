3b9e8bf209159a815a1525d078a0aaaa9381518f
Md-Asikul-Islam <mdasikuli48@gmail.com>
2023-09-24 15:43:27 +0600

    new line

--- Javascript_Advance/script.js
+++ Javascript_Advance/script.js
@@ -1,3 +1,112 @@
-১। Execution context 
+**** Functinal programming in javascript ***
 
-২।  Event Loop 
+১। First Class Object
+
+
+
+
+
+২। Pure Function
+
+
+
+
+
+
+৩। Higher Order Function
+
+
+
+
+
+৪। Composition
+
+
+
+
+৫। Declarative vs Imperative 
+
+
+
+
+
+৬। immutability 
+
+
+
+
+
+
+
+
+
+
+// **** Object Oriented Programmig in javascript ****
+
+
+১।  Class
+
+
+
+
+
+
+
+
+
+
+২।  inheritance or Reusability 
+
+
+
+
+
+
+
+৩। Encapsulation
+
+
+
+
+
+
+৪। Abstraction
+
+
+
+
+
+
+
+৫।  Pholymorphism
+
+
+
+
+
+
+
+
+৬।  Association 
+
+
+
+
+
+
+
+
+
+
+
+৭। Aggreggation 
+
+
+
+
+
+
+
+
+
+
